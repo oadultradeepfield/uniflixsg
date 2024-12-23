fb80121d9ab565a5dc0d2a9cced46c01f26465a0
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2024-12-23 10:08:30 +0700

    update cors policy

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,23 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 from database.migrate import migrate_data_to_db
 from routes.program import get_program_recommendations
 
+load_dotenv()
+
 app = Flask(__name__)
+
 migrate_data_to_db()
+
+allowed_origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
+if not allowed_origins:
+    print("Warning: No allowed origins specified in .env file")
+
+CORS(app, resources={r"/*": {"origins": allowed_origins}})
 
 
 @app.route("/api/recommend", methods=["POST"])

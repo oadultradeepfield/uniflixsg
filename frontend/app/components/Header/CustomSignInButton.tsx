f4613aaed5479d00ca3ed15a1edfcb2a5b94c0b5
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2024-12-23 15:02:10 +0700

    temporary remove frontend

--- frontend/app/components/Header/CustomSignInButton.tsx
+++
@@ -1,9 +0,0 @@
-import { SignInButton } from "@clerk/nextjs";
-
-export default function CustomSignInButton() {
-  return (
-    <SignInButton>
-      <button className="btn btn-secondary">Sign In</button>
-    </SignInButton>
-  );
-}
--- frontend/app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <div className="mx-auto flex h-screen items-center justify-center bg-base-200">
-      <SignIn />
-    </div>
-  );
-}
